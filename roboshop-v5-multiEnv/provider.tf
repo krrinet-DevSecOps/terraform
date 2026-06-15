c3aeb12fea9b490ef2a1729d8a30daa74fe7a468
ranjithii <indranstars@gmail.com>
2026-06-15 01:07:26 -0400

    Update provider.tf

--- roboshop-v5-multiEnv/provider.tf
+++ roboshop-v5-multiEnv/provider.tf
@@ -1,2 +1,8 @@
 # Configure the AWS Provider
 provider "aws" {}
+
+provider "vault" {
+  address = "http://vault-tools.krrinet.online:8200"
+  token   = var.vault_token 
+  skip_tls_verify = true
+}
