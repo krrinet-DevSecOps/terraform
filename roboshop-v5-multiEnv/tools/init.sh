c57c148caf6ec32b13d67942d9326c915c982f4d
ranjithii <indranstars@gmail.com>
2026-06-16 13:34:36 -0400

    Update init.sh

--- roboshop-v5-multiEnv/tools/init.sh
+++ roboshop-v5-multiEnv/tools/init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-token=$1
+#token=$1
 rm -rf .terraform ;
 git pull ;
 terraform init ;
-terraform plan -var vault_token=$token ; 
-terraform apply -auto-approve -var vault_token=$token
+terraform plan ; 
+terraform apply -auto-approve
