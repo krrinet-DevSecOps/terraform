5e800532d8076ed2bc536e108f15a38d58446d6e
ranjithii <indranstars@gmail.com>
2026-06-16 13:51:40 -0400

    Update locals.tf

--- roboshop-v5-multiEnv/modules/locals.tf
+++ roboshop-v5-multiEnv/modules/locals.tf
@@ -1,5 +1,5 @@
 locals {
   get_instance_ip = var.internal ? aws_instance.main.private_ip : aws_instance.main.public_ip
-  get_ssh_user = data.vault_generic_secret.ssh_cred.data["ssh_user"]
-  get_ssh_pass = data.vault_generic_secret.ssh_cred.data["ssh_pass"]
+  #get_ssh_user = data.vault_generic_secret.ssh_cred.data["ssh_user"]
+  #get_ssh_pass = data.vault_generic_secret.ssh_cred.data["ssh_pass"]
 }
